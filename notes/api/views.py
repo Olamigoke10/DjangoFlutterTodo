640ca9b0d09aba299356a90e812692e173823d84
MRBOX <olamigokeadebayo90@gmail.com>
2024-10-24 22:20:55 +0100

    STarting the django app

---
+++ notes/api/views.py
@@ -0,0 +1,45 @@
+from django.http import JsonResponse
+
+def getRoutes(request):
+    routes = [
+        {
+            "Endpoint": "/notes/",
+            "method": "/GET/",
+            "body": None,
+            "description": "Returns an array of notes"
+        },
+        {
+            "Endpoint": "/notes/id",
+            "method": "GET",
+            "body": None,
+            "description": "Returns a single note object"
+        },
+        {
+            "Endpoint": "/notes/",
+            "method": "GET",
+            "body": None,
+            "description": "Returns an array of notes"
+        },
+        
+        {
+            "Endpoint": "/notes/create/",
+            "method": "POST",
+            "body": {"body": ""},
+            "description": "Creates new notes sent in post request"
+        }, 
+        
+        {
+            "Endpoint": "/notes/update/",
+            "method": "PUT",
+            "body": {"body": ""},
+            "description": "Creates an existing note with data sent in"
+        }, 
+        
+        {
+            "Endpoint": "/notes/id/delete",
+            "method": "DELETE",
+            "body": None,
+            "description": "Deletes and exiting note"
+        },
+    ]
+    return JsonResponse(routes, safe=False)
